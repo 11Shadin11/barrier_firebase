8a53e4e710e5d7e11c217a90d0956922ab05e83d
Shadin <buhbuhbabah@gmail.com>
2023-12-05 23:40:58 +0400

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,98 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    .form(v-if="showForm")
+      <input ref="q" placeholder="Имя" type="text">
+      <input ref="w" placeholder="Фамилия" type="text">
+      <input ref="e" placeholder="Отчество" type="text">
+      <input ref="r" placeholder="Марка и модель авто" type="text">
+      <input ref="t" placeholder="Регистрационный знак авто" type="text">
+      <input ref="y" placeholder="Номер телефона" type="text">
+      <input ref="u" placeholder="Номер квартиры" type="text">
+      <select ref="i" placeholder="Статус жильца" type="text">
+        <option value="Владелец" selected>Владелец</option>
+        <option value="Арендатор">Арендатор</option>
+      </select>
+
+      button.addUser(@click="addNewMessage" fab) Добавить
+    .addUser(v-if="!showForm" @click="showForm = true") Добавить пользователья
+    .allUsers
+      .content-users(v-for="user in allUsers")
+        .users
+          b {{user.lastName}} {{user.firstName}} {{user.middleName}}
+          b {{user.status}}
+          b {{user.carInfo}}
+          b {{user.carRegNumber}}
+          b № Кв: {{user.appartmentNumber}}
+          b Тел. : {{user.phoneNuber}}
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {getFirestore, onSnapshot, collection, addDoc, query} from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import {ref,onUnmounted} from 'vue';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDGiuTr7FlQRZXuYaR4aqNq2QWi0IQYhKY",
+  authDomain: "barrier-5acd6.firebaseapp.com",
+  projectId: "barrier-5acd6",
+  storageBucket: "barrier-5acd6.appspot.com",
+  messagingSenderId: "604370511919",
+  appId: "1:604370511919:web:8cb4cf34cb69895e903eda",
+  measurementId: "G-BBZDHDYXR9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      showForm: false,
+      allUsers:ref([])
+    }
+  },
+
+  methods: {
+
+    addNewMessage:function() {
+      addDoc(collection(db,'residents'),{
+        firstName: this.$refs.q.value,
+        lastName: this.$refs.w.value,
+        middleName: this.$refs.e.value,
+        carInfo: this.$refs.r.value,
+        carRegNumber: this.$refs.t.value,
+        phoneNuber: this.$refs.y.value,
+        appartmentNumber: this.$refs.u.value,
+        status: this.$refs.i.value,        
+      });
+
+      console.log(this.allUsers);
+    },
+
+  },
+
+  mounted() {
+    const latestQuery = query(collection(db,"residents"));
+    const livemessages = onSnapshot(latestQuery,(snapshot)=>{
+      this.allUsers = snapshot.docs.map((doc) => {
+        console.log(doc);
+        return {
+          appartmentNumber: doc._document.data.value.mapValue.fields.appartmentNumber.stringValue,
+          carInfo: doc._document.data.value.mapValue.fields.carInfo.stringValue,
+          carRegNumber: doc._document.data.value.mapValue.fields.carRegNumber.stringValue,
+          firstName: doc._document.data.value.mapValue.fields.firstName.stringValue,
+          lastName: doc._document.data.value.mapValue.fields.lastName.stringValue,
+          middleName: doc._document.data.value.mapValue.fields.middleName.stringValue,
+          phoneNuber: doc._document.data.value.mapValue.fields.phoneNuber.stringValue,
+          status: doc._document.data.value.mapValue.fields.status
+        .stringValue}
+      });
+    });
+    onUnmounted(livemessages)
+
+    console.log(this.allUsers);
   }
 }
 </script>
@@ -24,5 +105,53 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+.content-users{
+  display:flex;
+  justify-content:center;
+}
+.users b {
+  margin-top: auto;
+  margin-bottom: auto;
+  color: #000;
+}
+.users{
+  cursor: pointer;
+  display:flex;
+  flex-wrap: wrap;
+  justify-content:space-between;
+  padding:10px;
+  margin: 10px;
+  width: 80%;
+  background: rgb(156, 156, 156);
+  border-radius:20px;
+  height: 50px;
+}
+.users:hover{
+  box-shadow: 0 5px 20px 2px #000;
+}
+.addUser{
+  position: fixed;
+  z-index: 100000;
+}
+.addUser {
+  width: max-content;
+  height: 40px;
+  border: 1px solid #004790;
+  border-radius: 5px;
+  padding: 5px 15px;
+  color: #004790;
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Add New Data</h1>
+    <form>
+      <label for="dataInput">New Data:</label>
+      <input type="text" id="dataInput" v-model="newData" />
+      <button type="submit" @click="addData">Add Data</button>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  data() {
+    return {
+      newData: ''
+    };
+  },
+  methods: {
+    addData() {
+      // Получаем ссылку на корень базы данных
+      const db = firebase.database();
+      const dataRef = db.ref('data');
+
+      // Добавляем новое поле в базу данных
+      dataRef.push({
+        value: this.newData
+      });
+
+      // Очищаем поле ввода после добавления данных
+      this.newData = '';
+    }
+  }
+};
+</script>
